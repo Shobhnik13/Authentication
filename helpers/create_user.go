0776c93a4df2a35389e60201c00a905712e6a96d
shobhnik13 <shobhnikw@gmail.com>
2025-10-21 14:09:54 +0545

    built helper functions

--- helpers/create_user.go
+++ helpers/create_user.go
@@ -4,8 +4,10 @@ import (
 	"auth/config"
 	"auth/models"
 	"context"
+	"errors"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -29,5 +31,50 @@ func CreateUser(user models.User) (models.User, error) {
 
 	user.ID = oid.Hex()
 	// fmt.Println("User created with ID:", user.ID)
+	return user, nil
+}
+
+func GetUserByEmail(email string) (models.User, error) {
+	collection := config.DB.Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var user models.User
+
+	// create filter for search
+	filter := bson.M{"email": email}
+
+	res := collection.FindOne(ctx, filter)
+
+	// now decoding the result into user
+	err := res.Decode(&user)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func GetUserByID(id string) (models.User, error) {
+	collection := config.DB.Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var user models.User
+
+	// convert string id to ObjectID for matching on mongodb indexed field of _id
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return models.User{}, errors.New("invalid user ID")
+	}
+
+	// create filter for search
+	filter := bson.M{"_id": oid}
+	res := collection.FindOne(ctx, filter)
+
+	// now decoding the result into user
+	err = res.Decode(&user)
+	if err != nil {
+		return user, err
+	}
 	return user, nil
 }
